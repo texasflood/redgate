9e5afee76e7758fd34222687c8b96541c67f95e3
Anas Syed <anas27@gmail.com>
2014-09-07 11:45:47 +0100

    Started adding class oriented version

--- WordCount.java
+++ WordCount.java
@@ -4,15 +4,15 @@ import java.util.*;
 
 public class WordCount 
 {
-    private static Vector<String> words = new Vector<String>(0, 1);
-    private static Vector<Integer> wordFreqs = new Vector<Integer>(0, 1);
     public static void main(String[] args)
     {
         SimpleCharacterReader charRead = new SimpleCharacterReader();
         wordCounter(charRead);
     }
+
     private static void wordCounter(SimpleCharacterReader charRead)
     {
+        List<WordData> wordsList = new ArrayList<WordData>();
         String buffer = new String("");
         boolean finished = false;
         while (!finished)
@@ -29,7 +29,6 @@ public class WordCount
                 {
                     if (buffer.length() != 0)
                     {
-                        addToWords(buffer);
                         buffer = "";
                     }
                 }
@@ -38,36 +37,12 @@ public class WordCount
             {
                 if (buffer.length() != 0)
                 {
-                    addToWords(buffer);
                 }
                 System.out.println("Finished Reading File");
-                System.out.println(words);
-                System.out.println(wordFreqs);
-                System.out.println("words size: " + words.capacity());
-                System.out.println("wordFreqs size: " + wordFreqs.capacity());
-                words = new Vector<String>(0, 1);
-                wordFreqs = new Vector<Integer>(0, 1);
                 finished = true;
             }
         }
     }
-    private static void addToWords(String buffer)
-    {
-        buffer = buffer.toLowerCase();
-        boolean foundBuffer = false;
-        for (int i = 0; i < words.capacity(); i++)
-        {
-            if ((words.elementAt(i)).equals(buffer))
-            {
-                wordFreqs.setElementAt(wordFreqs.elementAt(i) + 1, i);
-                foundBuffer = true;
-            }
-        }
-        if (!foundBuffer)
-        {
-            words.addElement(buffer);
-            wordFreqs.addElement(1);
-        }
-    }
 
 }
+
